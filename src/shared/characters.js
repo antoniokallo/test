98e10508e1e934f57e564263e4c102123221ca58
antoniokallo <antoniokallo@gmail.com>
2022-08-23 09:33:08 -0400

    smocp

---
+++ src/shared/characters.js
@@ -0,0 +1,22 @@
+export const playerStats = {
+  level: 44,
+  maxHealth: 177,
+  name: 'cuphed',
+  img: '/assets/cuphed.png',
+
+  magic: 187,
+  attack: 50,
+  defense: 30,
+  magicDefense: 30,
+};
+export const opponentStats = {
+  level: 44,
+  name: 'verizon guy',
+  maxHealth: 188,
+  img: '/assets/verizon.png',
+
+  magic: 50,
+  attack: 32,
+  defense: 20,
+  magicDefense: 48,
+};
