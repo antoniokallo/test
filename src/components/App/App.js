f66384dac3aa5dbaa664583823933fc1ec148b38
antoniokallo <antoniokallo@gmail.com>
2022-08-24 09:30:15 -0400

    2com

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import styles from './styles.module.css';
 import { useEffect, useState } from 'react';
 import { Battle, EndMenu, StartMenu } from 'components';
-import { Routes, routes } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 export const App = () => {
   const [winner, setWinner] = useState();
@@ -14,8 +14,28 @@ export const App = () => {
   }, [mode]);
 
   return (
-    <Routes></Routes>
+   
     <div className={styles.main}>
+    {/* <nav>
+      <ul>
+      <li>
+        <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/StartMenu">Contacts</Link>
+        </li>
+        <li>
+        <Link to="/Battle">battle</Link>
+        </li>
+       
+      </ul>
+    </nav>
+    
+      <Routes>
+        <Route path='/StartMenu' element={<StartMenu />}/>
+        <Route path='/Battle' element={<Battle />}/>
+        
+      </Routes> */}
       {mode === 'start' && (
         <StartMenu onStartClick={() => setMode('battle')} />
       )}
@@ -32,6 +52,29 @@ export const App = () => {
       {mode === 'gameOver' && !!winner && (
         <EndMenu winner={winner} onStartClick={() => setMode('battle')} />
       )}
+
+<nav classname={styles.secondary}>
+      <ul>
+      <button>
+        <Link to="/">home</Link>
+        </button>
+        <button>
+          <Link to="/StartMenu">Contacts</Link>
+        </button>
+        <button>
+        <Link to="/Battle">battle</Link>
+        </button>
+       
+      </ul>
+    </nav>
+    
+      <Routes>
+        <Route path='/StartMenu' element={<StartMenu />}/>
+        <Route path='/Battle' element={<Battle />}/>
+        
+      </Routes>
+
     </div>
+    
   );
 };
